2d22a39e77b59d719a48e02f02531495e5a5b1dd
jyoothish <jyoothishh@gmail.com>
2025-10-19 11:47:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-yellow-300">28 Card Game</h1>
+      <p className="mt-4 text-lg">Setup Complete ✅</p>
+    </div>
+  )
+}
+
+export default App
